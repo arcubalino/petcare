61f28b5ccb2678e93a0e5942ed4469bbbad18b2d
Ranil Jaramillo <ran.revolution@gmail.com>
2017-02-19 21:15:37 +0800

    NgCordoval added

--- www/js/app.js
+++ www/js/app.js
@@ -3,15 +3,140 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
 
-.controller("AppCtrl",function($scope,$http){
+// .controller("AppCtrl",function($scope,$http){
 
-  $scope.title = "ahoi"
+//   $scope.title = "ahoi"
+// })
+
+.controller('ImageCtrl', function ($scope, $cordovaCamera, $cordovaFile, $cordovaFileTransfer, $cordovaDevice, $ionicPopup, $cordovaActionSheet) {
+  $scope.image = null;
+ 
+  $scope.showAlert = function(title, msg) {
+    var alertPopup = $ionicPopup.alert({
+      title: title,
+      template: msg
+    });
+  };
+
+
+  $scope.loadImage = function() {
+  var options = {
+    title: 'Select Image Source',
+    buttonLabels: ['Load from Library', 'Use Camera'],
+    addCancelButtonWithLabel: 'Cancel',
+    androidEnableCancelButton : true,
+  };
+  $cordovaActionSheet.show(options).then(function(btnIndex) {
+    var type = null;
+    if (btnIndex === 1) {
+      type = Camera.PictureSourceType.PHOTOLIBRARY;
+    } else if (btnIndex === 2) {
+      type = Camera.PictureSourceType.CAMERA;
+    }
+    if (type !== null) {
+      $scope.selectPicture(type);
+    }
+  });
+};
+
+// Take image with the camera or from library and store it inside the app folder
+// Image will not be saved to users Library.
+$scope.selectPicture = function(sourceType) {
+  var options = {
+    quality: 100,
+    destinationType: Camera.DestinationType.FILE_URI,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false
+  };
+ 
+  $cordovaCamera.getPicture(options).then(function(imagePath) {
+    // Grab the file name of the photo in the temporary directory
+    var currentName = imagePath.replace(/^.*[\\\/]/, '');
+ 
+    //Create a new name for the photo
+    var d = new Date(),
+    n = d.getTime(),
+    newFileName =  n + ".jpg";
+ 
+    // If you are trying to load image from the gallery on Android we need special treatment!
+    if ($cordovaDevice.getPlatform() == 'Android' && sourceType === Camera.PictureSourceType.PHOTOLIBRARY) {
+      window.FilePath.resolveNativePath(imagePath, function(entry) {
+        window.resolveLocalFileSystemURL(entry, success, fail);
+        function fail(e) {
+          console.error('Error: ', e);
+        }
+ 
+        function success(fileEntry) {
+          var namePath = fileEntry.nativeURL.substr(0, fileEntry.nativeURL.lastIndexOf('/') + 1);
+          // Only copy because of access rights
+          $cordovaFile.copyFile(namePath, fileEntry.name, cordova.file.dataDirectory, newFileName).then(function(success){
+            $scope.image = newFileName;
+          }, function(error){
+            $scope.showAlert('Error', error.exception);
+          });
+        };
+      }
+    );
+    } else {
+      var namePath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      // Move the file to permanent storage
+      $cordovaFile.moveFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(function(success){
+        $scope.image = newFileName;
+      }, function(error){
+        $scope.showAlert('Error', error.exception);
+      });
+    }
+  },
+  function(err){
+    // Not always an error, maybe cancel was pressed...
+  })
+};
+
+
+$scope.pathForImage = function(image) {
+  if (image === null) {
+    return '';
+  } else {
+    return cordova.file.dataDirectory + image;
+  }
+};
+
+
+$scope.uploadImage = function() {
+  // Destination URL
+  var url = "http://192.168.43.78/petcare/upload.php";
+ 
+  // File for Upload
+  var targetPath = $scope.pathForImage($scope.image);
+ 
+  // File name only
+  var filename = $scope.image;;
+ 
+  var options = {
+    fileKey: "file",
+    fileName: filename,
+    chunkedMode: false,
+    mimeType: "multipart/form-data",
+    params : {'fileName': filename}
+  };
+ 
+  $cordovaFileTransfer.upload(url, targetPath, options).then(function(result) {
+    $scope.showAlert('Success', 'Image upload finished.');
+  });
+}
+
+
+
+
+
+
+
+ 
+ //===================== The rest of the app comes in here
 })
-
-
 
 
 .run(function($ionicPlatform) {
